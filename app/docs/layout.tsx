65053234321f70ea412f2e62311d0a866168f770
gaojunran <nebula2021@126.com>
2025-07-17 14:43:16 +0800

    feat!: init

---
+++ app/docs/layout.tsx
@@ -0,0 +1,22 @@
+import { DocsLayout, type DocsLayoutProps } from 'fumadocs-ui/layouts/notebook';
+import type { ReactNode } from 'react';
+import { baseOptions } from '@/app/layout.config';
+import { source } from '@/lib/source';
+import { GithubInfo } from 'fumadocs-ui/components/github-info';
+
+const docsOptions: DocsLayoutProps = {
+  ...baseOptions,
+  tree: source.pageTree,
+  links: [
+    {
+      type: 'custom',
+      children: (
+        <GithubInfo owner="gaojunran" repo="frontend-interview" token={process.env.GITHUB_TOKEN} className="lg:-mx-2" />
+      ),
+    },
+  ],
+};
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return <DocsLayout {...docsOptions}>{children}</DocsLayout>;
+}
